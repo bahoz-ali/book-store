5c8d912bb467d9e37ef8845ce2b65fdb1837fd69
Bahoz <bahozali50@gmail.com>
2022-08-12 23:26:24 +0300

    setup the react-route-dom, and give basic styles, and create structor folder and components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import './reset.css'
+import Header from './components/Header';
+import Categories from './pages/Categories';
+import Home from './pages/Home';
+import './reset.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
-   </div>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </div>
   );
 }
 
