0a3583be875ec13ccc45d0e45bcc9d57f65c10cb
Bahoz <bahozali50@gmail.com>
2022-08-18 21:42:27 +0300

    using dispatch to add new book in the form component

--- src/components/FormBook.jsx
+++ src/components/FormBook.jsx
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
 const FormBook = () => {
+  const dispatch = useDispatch();
+
+  const [bookData, setBookData] = useState({
+    title: '',
+    author: '',
+  });
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+
+    setBookData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const { title, author } = bookData;
+
+    if (!title.trim() || !author.trim()) return;
+
+    const newBook = {
+      title,
+      author,
+      id: uuidv4(),
+    };
+
+    dispatch(addBook(newBook));
+  };
+
   return (
     <section className="form-section">
       <div className="form__header">
         <h1>ADD NEW BOOK</h1>
       </div>
 
-      <form className="from">
+      <form className="from" onSubmit={submitHandler}>
         <input
           type="text"
           placeholder="Book title"
-          name="book_title"
-          id="book_title"
+          name="title"
+          value={bookData.title}
+          onChange={changeHandler}
+          id="title"
         />
-        
+
         <input
           type="text"
           placeholder="Book Author"
-          name="book_author"
-          id="book_author"
+          name="author"
+          id="author"
+          onChange={changeHandler}
+          value={bookData.author}
         />
+
+        <button type="submit">Add Book</button>
       </form>
     </section>
   );
