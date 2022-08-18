ac4c949d50daf44193643b90b619fca72ce9a9db
Bahoz <bahozali50@gmail.com>
2022-08-18 22:14:47 +0300

    handle the checkStatus in category page

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 const Categories = () => {
-  return <div><button>Check status</button></div>;
+  const dispatch = useDispatch();
+  const status = useSelector((state) => {
+    return state.categories;
+  });
+
+  const handleCheckStatus = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <div>
+      <button onClick={handleCheckStatus}>Check status</button>
+      <p>{status}</p>
+    </div>
+  );
 };
 
 export default Categories;
