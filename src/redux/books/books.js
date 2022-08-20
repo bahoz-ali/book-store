6b5208fc0d03991c78adcf393705dd016d2a0e57
Bahoz <bahozali50@gmail.com>
2022-08-20 12:16:19 +0300

    update the action creators using creatAsyncThunk(type , async ()=>{})"

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,9 @@
-const ADD = 'bookstore/books/ADD_BOOK';
-const REMOVE = 'bookstore/books/REMOVE_BOOK';
-const FETCH_BOOKS = 'bookstore/books/FETCH_BOOKS'
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import API from '../../service.js';
+
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const FETCH_BOOKS = 'bookstore/books/FETCH_BOOKS';
 
 const initialState = [
   {
@@ -23,11 +26,11 @@ const initialState = [
 // book reducer
 export default (state = initialState, action) => {
   switch (action.type) {
-     case `${FETCH_BOOKS}/fulfilled`:
+    case `${FETCH_BOOKS}/fulfilled`:
       return { ...state, books: [...action.payload] };
-    case `${ADD}/fulfilled`:
+    case `${ADD_BOOK}/fulfilled`:
       return [...state, action.book];
-    case `${REMOVE}/fulfilled`:
+    case `${REMOVE_BOOK}/fulfilled`:
       return state.filter((book) => book.id !== action.id);
     default:
       return state;
@@ -35,5 +38,44 @@ export default (state = initialState, action) => {
 };
 
 // action creators
-export const addBook = (book) => ({ type: ADD, book });
-export const removeBook = (id) => ({ type: REMOVE, id });
+
+export const getBooks = createAsyncThunk(FETCH_BOOKS, async () => {
+  const response = await fetch(API.booksEndPoint);
+  const data = await response.json();
+
+  const books = [];
+
+  for (const key in data) {
+    const { title, author, category } = data[key][0];
+
+    const book = {
+      item_id: key,
+      title,
+      author,
+      category,
+    };
+
+    books.push(book);
+  }
+
+  return books;
+});
+
+export const addBook = createAsyncThunk(ADD_BOOK, async (book) => {
+  await fetch(API.booksEndPoint, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(book),
+  });
+
+  return book;
+});
+
+export const removeBook = createAsyncThunk(REMOVE_BOOK, async (id) => {
+  await fetch(API.booksEndPoint + '/' + id, {
+    method: 'DELETE',
+    body: { item_id: id },
+  });
+
+  return id;
+});
