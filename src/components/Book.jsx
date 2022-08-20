3f8cd12166bbb1ab6157158eb358d564bbe36da9
Bahoz <bahozali50@gmail.com>
2022-08-20 20:29:58 +0300

    styling the book

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -5,8 +5,19 @@ import { removeBook } from '../redux/books/books';
 import Circle from './Circle';
 
 const Book = (props) => {
+  return (
+    <section className="book-component">
+      <BookMain bookData={props.bookData} />
+      <Circle />
+      <div className="Line large"></div>
+      <BookChapter />
+    </section>
+  );
+};
+
+function BookMain(props) {
   const dispatch = useDispatch();
-  const { author, title, item_id } = props.bookData;
+  const { author, title, item_id, category } = props.bookData;
 
   const deleteHandler = (id) => {
     if (!id) return;
@@ -15,23 +26,50 @@ const Book = (props) => {
   };
 
   return (
-    <section className="book-component">
-      <h3>{title}</h3>
-      <p>{author}</p>
-      <button
-        className="btn btn--primary"
-        onClick={() => deleteHandler(item_id)}
-        type="button">
-        Delete
-      </button>
+    <div className="book">
+      <div className="book-info__header">
+        <p className="book__genre">{category}</p>
+        <h2 className="book__title">{title}</h2>
+        <p className="book__author">{author}</p>
+      </div>
 
-      <Circle />
-    </section>
+      <div className="book__footer">
+        <button className="btn btn--secondary" type="button">
+          Comment
+        </button>
+
+        <div className="Line line--small"></div>
+
+        <button
+          className="btn btn--secondary"
+          onClick={() => deleteHandler(item_id)}
+          type="button">
+          Remove
+        </button>
+
+        <div className="Line line--small"></div>
+
+        <button className="btn btn--secondary" type="button">
+          Edit
+        </button>
+      </div>
+    </div>
   );
-};
+}
 
+function BookChapter() {
+  return (
+    <div className="book-chapter">
+      <div>
+        <p className="current__chapter">current chapter</p>
+        <h2 className="chapter__number">chapter 18</h2>
+      </div>
 
-
-
+      <button className="btn btn--primary" type="button">
+        update progress
+      </button>
+    </div>
+  );
+}
 
 export default Book;
--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
+const style = {
+  display: 'flex',
+  gap: '1rem',
+};
+
 function Circle() {
   const percentage = Math.floor(Math.random() * 101);
 
   return (
-    <div style={{ width: 100, height: 100 }}>
-      <CircularProgressbar value={percentage} text={`${percentage}%`} />
+    <div className="circle-component">
+      <div style={{ width: 50, height: 50 }}>
+        <CircularProgressbar value={percentage} />
+      </div>
+
+      <div>
+        <h2>{percentage}%</h2>
+        <p>{percentage == 100 ? 'Completed' : 'Not completed'}</p>
+      </div>
     </div>
   );
 }
